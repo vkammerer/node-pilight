505c24ea636b84be524010069aeb9a6e62e97646
Vincent Kammerer <vkammerer@gmail.com>
2013-12-28 23:55:29 +0000

    a bit better

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 var exec = require('child_process').exec,
-		light,
-		status;
+		fs = require('fs'),
+		Q =  require('q'),
+		lightRef,
+		lightStatus;
 
-process.argv.forEach(function (val, index, array) {
-	if (val.match(/^light:/)) {
-		light = val.substring(6)
-	  console.log('Light: ' + light);
+var qLights = Q.defer();
+
+fs.readFile('./lights.json', function (err, data) {
+  if (err) throw err;
+	qLights.resolve(JSON.parse(data));
+});		
+
+var getArgs = function(){
+	var	toReturn = {};
+	process.argv.forEach(function (val, index, array) {
+		if (val.match(/^light:/)) {
+			toReturn.ref = val.substring(6)
+		}
+		else if (val.match(/^status:/)) {
+			toReturn.status = val.substring(7)
+		}
+	});
+	return toReturn;
+}
+
+var args = getArgs();
+
+qLights.promise.then(function (lights) {
+	if (lights[args.ref][args.status]) {
+		switchLight(lights[args.ref][args.status]);
 	}
-	if (val.match(/^status:/)) {
-		status = val.substring(7)
-	  console.log('Status: ' + status);
-	}
+}, function (reason) {
+	console.log('Lights configuration file not read: ' + reason)
 });
 
 
-
-/*
-
-exec('sudo /home/pi/Development/pilight/pilight',
-  function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }
-});
-
-*/
+var switchLight = function(command){
+	exec('sudo pilight-send -p raw -c "' + command + '"',
+	  function (error, stdout, stderr) {
+	    console.log('stdout: ' + stdout);
+	    console.log('stderr: ' + stderr);
+	    if (error !== null) {
+	      console.log('exec error: ' + error);
+	    }
+	});
+}
